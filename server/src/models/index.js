d0125eea8690cd78eda31bab16aca03050740765
james.anderson <anderson.james_@outlook.com>
2024-04-27 19:33:47 +0100

    lint and push all

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -7,7 +7,7 @@ const db = {}
 let sequelize
 
 if (process.env.DEPLOYED === 'True') {
-  sequelize = new Sequelize('postgresql://postgres:OZSWLEHAjcotbDxDnenUkEABzIjcVrHl@viaduct.proxy.rlwy.net:45112/railway')
+  sequelize = new Sequelize(process.env.POSTGRES_URL)
 } else {
   sequelize = new Sequelize(
     config.db.database,
