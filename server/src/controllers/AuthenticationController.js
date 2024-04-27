8dd6baf3b191724b285bac91a1bc927376ce44a1
james.anderson <anderson.james_@outlook.com>
2024-04-27 21:51:31 +0100

    lint and push all

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -3,6 +3,8 @@ const { user } = require('../models')
 module.exports = {
   async register (req, res) {
     console.log(`Register End Point Hit. With Request: ${req.body.email}`)
+    console.log('Model User: ', user.create)
+
     const userData = await user.create(req.body)
     try {
       res.send(userData.toJSON())
