be9e4054529a1da41d5b1dec6d969d510b8fc1aa
james.anderson <anderson.james_@outlook.com>
2024-04-27 09:36:24 +0100

    root API path

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,30 @@
 console.log('hello')
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: `status pinged!`
-    })
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello There, are you ready to code!'
+  })
 })
 
-
-
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'status pinged!'
+  })
+})
 
 app.post('/register', (req, res) => {
-    res.send({
-        message: `hello ${req.body.email}!`
-    })
+  res.send({
+    message: `hello ${req.body.email}!`
+  })
 })
 
-app.listen(process.env.PORT || 8081) 
+app.listen(process.env.PORT || 8081)
